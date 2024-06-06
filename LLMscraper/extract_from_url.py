88dc9c2abe9f4d33d00a6f174d9362a1304ab208
Efraim <none>
2024-06-06 10:44:07 -0400

    Added extract_from_url script -- currently as placeholder.

---
+++ LLMscraper/extract_from_url.py
@@ -0,0 +1,12 @@
+"""
+Script to scrape parameters
+"""
+import LLMscraper
+
+class Parameter():
+    name        : str
+    description : str
+
+
+def extract_parameter( url : str, param : Parameter ) :
+    pass
