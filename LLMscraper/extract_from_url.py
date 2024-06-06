3c0e5a01e13be37932ca1925ada8df568207e937
Efraim <none>
2024-06-06 15:16:14 -0400

    Added code to extract_parameter to use LLM to parse a url.

--- LLMscraper/extract_from_url.py
+++ LLMscraper/extract_from_url.py
@@ -1,12 +1,31 @@
 """
 Script to scrape parameters
 """
-import LLMscraper
+from LLMAccessor import LLMAccessor
+import requests
 
 class Parameter():
     name        : str
     description : str
 
 
-def extract_parameter( url : str, param : Parameter ) :
-    pass
+
+def extract_parameter( url : str, param : Parameter )  :
+    request = requests.get( url )
+    text    = request.content
+    prompt  = f"Find the value of parameter which is defined as: {param.description} " \
+            + f" \n\n The following context contains the needed information: \n {text}"
+    
+    llm    = LLMAccessor()
+    answer = llm.generate( prompt )
+
+    return answer
+
+
+if( __name__ == "__main__" ) :
+    url = "https://www.pa.gov/en/agencies/dhs/resources/snap/snap-income-limits.html"
+    p = Parameter()
+    p.name = "IncomeLimit2"
+    p.description = "The maximum gross monthly income for a family of 2 to be eligible for SNAP benefits."
+    a = extract_parameter( url, p )
+    print( a )
